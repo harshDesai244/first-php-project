ce81c518fd2e9161180eda5bd6a94c1adca11983
harshDesai244 <harshdesai244@gmail.com>
2020-04-18 13:08:16 +0200

    first commit

---
+++ html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<div style="margin-left:600px; margin-top:200px">
+<form action="index.php " method="POST">
+<input type="text" id="user" placeholder="Enter Username" name="user"><br><br>
+<input type="password" id="pass" placeholder="Enter Password" name="password"><br><br>
+<input type="submit" value="login" name="submit">
+</div>
+</form>
+
+</body>
+</html>
